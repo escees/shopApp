b8e3d860b04bdb991fe06fd7e44ef10473b83c56
fade404 <fade@poczta.onet.pl>
2016-05-25 15:04:28 +0200

    Added FosUserBundle all settings

--- src/ShopAppBundle/Entity/User.php
+++ src/ShopAppBundle/Entity/User.php
@@ -37,19 +37,6 @@ class User extends BaseUser
      */
     private $surname;
 
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="email", type="string", length=255)
-     */
-    private $email;
-
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="password", type="string", length=255)
-     */
-    private $password;
 
     /**
      * @var string
@@ -126,51 +113,6 @@ class User extends BaseUser
         return $this->surname;
     }
 
-    /**
-     * Set email
-     *
-     * @param string $email
-     * @return User
-     */
-    public function setEmail($email)
-    {
-        $this->email = $email;
-
-        return $this;
-    }
-
-    /**
-     * Get email
-     *
-     * @return string 
-     */
-    public function getEmail()
-    {
-        return $this->email;
-    }
-
-    /**
-     * Set password
-     *
-     * @param string $password
-     * @return User
-     */
-    public function setPassword($password)
-    {
-        $this->password = $password;
-
-        return $this;
-    }
-
-    /**
-     * Get password
-     *
-     * @return string 
-     */
-    public function getPassword()
-    {
-        return $this->password;
-    }
 
     /**
      * Set address
